d81a0d34ab05f14df52d51085980a0024333bcda
HeraBeginners25 <gallerysingaraja8@gmail.com>
2025-12-10 20:28:54 +0800

    build

--- resources/views/detail-portfolio.blade.php
+++ resources/views/detail-portfolio.blade.php
@@ -7,7 +7,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    
+    {{-- fix front end  --}}
     <style>
         :root { --accent: {{ $setting?->theme_color ?? '#000000' }}; }
         .text-accent { color: var(--accent); }
